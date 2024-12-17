395fea8ac13a8f75159c96675c6fc991dec6cc8c
Aaron Powell <me@aaron-powell.com>
2024-12-17 14:43:23 +1100

    Adding support for Ollama and moving the Azure deps to publishing time.
    This means that you can run it locally without having to have provisioned any Azure resources

--- src/Products/Memory/MemoryContext.cs
+++ src/Products/Memory/MemoryContext.cs
@@ -1,30 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SearchEntities;
 using DataEntities;
-using OpenAI.Chat;
-using OpenAI.Embeddings;
 using VectorEntities;
 using Microsoft.Extensions.VectorData;
 using Newtonsoft.Json;
 using Products.Models;
+using Microsoft.Extensions.AI;
 
 namespace Products.Memory;
 
+public record ScoreThreshold(double Value);
+
 public class MemoryContext(
-    ILogger logger,
-    ChatClient chatClient,
-    EmbeddingClient embeddingClient,
-    IVectorStore vectorStore,
-    Context db)
+    ILogger<MemoryContext> logger,
+    IChatClient chatClient,
+    IEmbeddingGenerator<string, Embedding<float>> embeddingClient,
+    IVectorStoreRecordCollection<int, ProductVector> productsCollection,
+    Context db,
+    ScoreThreshold scoreThreshold)
 {
-    public IVectorStoreRecordCollection<int, ProductVector>? _productsCollection;
     private string _systemPrompt = "You are a useful assistant. You always reply with a short and funny message. If you do not know an answer, you say 'I don't know that.' You only answer questions related to outdoor camping products. For any other type of questions, explain to the user that you only answer outdoor camping products questions. Do not store memory of the chat conversation.";
 
     public async Task<bool> InitMemoryContextAsync()
     {
         logger.LogInformation("Initializing memory context");
-        _productsCollection = vectorStore.GetCollection<int, ProductVector>("products");
-        await _productsCollection.CreateCollectionIfNotExistsAsync();
+        await productsCollection.CreateCollectionIfNotExistsAsync();
 
         // define system prompt
         _systemPrompt = "You are a useful assistant. You always reply with a short and funny message. If you do not know an answer, you say 'I don't know that.' You only answer questions related to outdoor camping products. For any other type of questions, explain to the user that you only answer outdoor camping products questions. Do not store memory of the chat conversation.";
@@ -54,8 +54,8 @@ public class MemoryContext(
                 };
                 var result = await embeddingClient.GenerateEmbeddingAsync(productInfo);
 
-                productVector.Vector = result.Value.ToFloats();
-                var recordId = await _productsCollection.UpsertAsync(productVector);
+                productVector.Vector = result.Vector;
+                var recordId = await productsCollection.UpsertAsync(productVector);
                 logger.LogInformation("Product added to memory: {Product} with recordId: {RecordId}", product.Name, recordId);
             }
             catch (Exception exc)
@@ -78,7 +78,7 @@ public class MemoryContext(
         try
         {
             var result = await embeddingClient.GenerateEmbeddingAsync(search);
-            var vectorSearchQuery = result.Value.ToFloats();
+            var vectorSearchQuery = result.Vector;
 
             var searchOptions = new VectorSearchOptions()
             {
@@ -86,13 +86,12 @@ public class MemoryContext(
                 VectorPropertyName = "Vector"
             };
 
-            // search the vector database for the most similar product        
-            var searchResults = await _productsCollection.VectorizedSearchAsync(vectorSearchQuery, searchOptions);
-            double searchScore = 0.0;
-            string? responseText;
+            // search the vector database for the most similar product
+            var searchResults = await productsCollection.VectorizedSearchAsync(vectorSearchQuery, searchOptions);
+            string? responseText = null;
             await foreach (var searchItem in searchResults.Results)
             {
-                if (searchItem is not null && searchItem.Score > 0.5)
+                if (searchItem is not null && searchItem.Score > scoreThreshold.Value)
                 {
                     // product found, search the db for the product details
                     firstProduct = new Product
@@ -104,37 +103,51 @@ public class MemoryContext(
                         ImageUrl = searchItem.Record.ImageUrl
                     };
 
-                    searchScore = searchItem.Score.Value;
                     responseText = $"The product [{firstProduct.Name}] fits with the search criteria [{search}][{searchItem.Score.Value.ToString("0.00")}]";
                     logger.LogInformation("Search Response: {ResponseText}", responseText);
                 }
+
+
+                // let's improve the response message
+                var prompt = $"""
+                    You are an intelligent assistant helping clients with their search about outdoor products. Generate a catchy and friendly message using the following information:
+                        - User Question: {search}
+                        - Found Product Name: {firstProduct.Name}
+                        - Found Product Description: {firstProduct.Description}
+                        - Found Product Price: {firstProduct.Price}
+                    Include the found product information in the response to the user question.
+                """;
+
+                var messages = new List<ChatMessage>
+                {
+                    new(ChatRole.System, _systemPrompt),
+                    new(ChatRole.User, prompt)
+                };
+
+                logger.LogInformation("{ChatHistory}", JsonConvert.SerializeObject(messages));
+
+                var resultPrompt = await chatClient.CompleteAsync(messages);
+                responseText = resultPrompt.Message.Text;
             }
 
-            // let's improve the response message
-            var prompt = $"""
-                You are an intelligent assistant helping clients with their search about outdoor products. Generate a catchy and friendly message using the following information:
-                    - User Question: {search}
-                    - Found Product Name: {firstProduct.Name}
-                    - Found Product Description: {firstProduct.Description}
-                    - Found Product Price: {firstProduct.Price}
-                Include the found product information in the response to the user question.
-            """;
-
-            var messages = new List<ChatMessage>
+            if (firstProduct is null)
             {
-                new SystemChatMessage(_systemPrompt),
-                new UserChatMessage(prompt)
-            };
+                var messages = new List<ChatMessage>
+                {
+                    new(ChatRole.System, _systemPrompt),
+                    new(ChatRole.User, $"The search term: {search} did not match any products.")
+                };
 
-            logger.LogInformation("{ChatHistory}", JsonConvert.SerializeObject(messages));
+                logger.LogInformation("{ChatHistory}", JsonConvert.SerializeObject(messages));
 
-            var resultPrompt = await chatClient.CompleteChatAsync(messages);
-            responseText = resultPrompt.Value.Content[0].Text!;
+                var resultPrompt = await chatClient.CompleteAsync(messages);
+                responseText = resultPrompt.Message.Text;
+            }
 
             // create a response object
             response = new SearchResponse
             {
-                Products = firstProduct == null ? [new Product()] : [firstProduct],
+                Products = [firstProduct ?? new Product()],
                 Response = responseText
             };
 
--- src/Products/Program.cs
+++ src/Products/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.VectorData;
 using Microsoft.SemanticKernel.Connectors.InMemory;
 using OpenAI;
+using OpenAI.VectorStores;
 using Products.Endpoints;
 using Products.Memory;
 using Products.Models;
+using VectorEntities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,50 +21,69 @@ builder.Services.AddProblemDetails();
 // Add DbContext service
 builder.AddSqlServerDbContext<Context>("sqldb");
 
-// in dev scenarios rename this to "openaidev", and check the documentation to reuse existing AOAI resources
-var azureOpenAiClientName = "openai";
-builder.AddAzureOpenAIClient(azureOpenAiClientName);
-
-// get azure openai client and create Chat client from aspire hosting configuration
-builder.Services.AddSingleton(serviceProvider =>
+if (builder.Environment.IsDevelopment())
 {
-    var chatDeploymentName = "gpt-4o-mini";
-    var logger = serviceProvider.GetService<ILogger<Program>>()!;
-    logger.LogInformation("Chat client configuration, modelId: {chatDeploymentName}", chatDeploymentName);
-    try
-    {
-        OpenAIClient client = serviceProvider.GetRequiredService<OpenAIClient>();
-        return client.GetChatClient(chatDeploymentName);
-    }
-    catch (Exception exc)
-    {
-        logger.LogError(exc, "Error creating embeddings client");
-        throw;
-    }
-});
+    builder.AddOllamaSharpChatClient("chat");
+    builder.AddOllamaSharpEmbeddingGenerator("embeddings");
 
-// get azure openai client and create embedding client from aspire hosting configuration
-builder.Services.AddSingleton(serviceProvider =>
+    builder.Services.AddSingleton(new ScoreThreshold(0.4));
+}
+else
 {
-    var embeddingsDeploymentName = "text-embedding-ada-002";
-    var logger = serviceProvider.GetService<ILogger<Program>>()!;
-    logger.LogInformation("Embeddings client configuration, modelId: {embeddingsDeploymentName}", embeddingsDeploymentName);
-    try
+    // in dev scenarios rename this to "openaidev", and check the documentation to reuse existing AOAI resources
+    var azureOpenAiClientName = "openai";
+    builder.AddAzureOpenAIClient(azureOpenAiClientName);
+
+    // get azure openai client and create Chat client from aspire hosting configuration
+    builder.Services.AddSingleton(serviceProvider =>
     {
-        OpenAIClient client = serviceProvider.GetRequiredService<OpenAIClient>();
-        return client.GetEmbeddingClient(embeddingsDeploymentName);
-    }
-    catch (Exception exc)
+        var chatDeploymentName = "gpt-4o-mini";
+        var logger = serviceProvider.GetService<ILogger<Program>>()!;
+        logger.LogInformation("Chat client configuration, modelId: {chatDeploymentName}", chatDeploymentName);
+        try
+        {
+            OpenAIClient client = serviceProvider.GetRequiredService<OpenAIClient>();
+            return client.GetChatClient(chatDeploymentName).AsChatClient();
+        }
+        catch (Exception exc)
+        {
+            logger.LogError(exc, "Error creating embeddings client");
+            throw;
+        }
+    });
+
+    // get azure openai client and create embedding client from aspire hosting configuration
+    builder.Services.AddSingleton(serviceProvider =>
     {
-        logger.LogError(exc, "Error creating embeddings client");
-        throw;
-    }
-});
+        var embeddingsDeploymentName = "text-embedding-ada-002";
+        var logger = serviceProvider.GetService<ILogger<Program>>()!;
+        logger.LogInformation("Embeddings client configuration, modelId: {embeddingsDeploymentName}", embeddingsDeploymentName);
+        try
+        {
+            OpenAIClient client = serviceProvider.GetRequiredService<OpenAIClient>();
+            return client.GetEmbeddingClient(embeddingsDeploymentName).AsEmbeddingGenerator();
+        }
+        catch (Exception exc)
+        {
+            logger.LogError(exc, "Error creating embeddings client");
+            throw;
+        }
+    });
+
+    AppContext.SetSwitch("OpenAI.Experimental.EnableOpenTelemetry", true);
+    builder.Services.AddSingleton(new ScoreThreshold(0.8));
+}
 
 // add memory context
 builder.Services.AddScoped<MemoryContext>();
 
 builder.Services.AddSingleton<IVectorStore, InMemoryVectorStore>();
+
+builder.Services.AddSingleton(sp =>
+{
+    var vectorStore = sp.GetRequiredService<IVectorStore>();
+    return vectorStore.GetCollection<int, ProductVector>("products");
+});
 
 // Add services to the container.
 var app = builder.Build();
@@ -75,10 +97,6 @@ app.UseHttpsRedirection();
 app.MapProductEndpoints();
 
 app.UseStaticFiles();
-
-// log Azure OpenAI resources
-app.Logger.LogInformation("Azure OpenAI resources\n >> OpenAI Client Name: {azureOpenAiClientName}", azureOpenAiClientName);
-AppContext.SetSwitch("OpenAI.Experimental.EnableOpenTelemetry", true);
 
 // manage db
 using (var scope = app.Services.CreateScope())
